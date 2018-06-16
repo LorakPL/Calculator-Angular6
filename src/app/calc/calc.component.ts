63e0f8b7e8d38e7b9d76fa38a2685f74f0fd0a81
Karol Furmanski <karolfur1995@gmail.com>
2018-06-16 17:17:15 +0200

    added arithmetic operations

--- src/app/calc/calc.component.ts
+++ src/app/calc/calc.component.ts
@@ -6,10 +6,61 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calc.component.css']
 })
 export class CalcComponent implements OnInit {
+  result = '0';
+  memoryResult: number;
+  operator: string;
+  error = 'Błąd';
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  sendValue(value: any) {
+    this.result += value;
+  }
+
+  insertNumber(value: any) {
+    if (this.result === '+' || this.result === '-' || this.result === '×' || this.result === '/' || this.result === this.error || this.result === '0') {
+      this.result = value;
+    } else {
+      this.result += value;
+    }
+  }
+
+  arithmeticOperation(operator: string) {
+    this.operator = operator;
+    this.memoryResult = Number(this.result);
+    this.result = operator;
+  }
+
+  showResult() {
+    if (this.operator === '+') {
+      this.memoryResult = Number(this.memoryResult) + Number(this.result);
+    } else if (this.operator === '-') {
+      this.memoryResult = Number(this.memoryResult) - Number(this.result);
+    } else if (this.operator === '×') {
+      this.memoryResult = Number(this.memoryResult) * Number(this.result);
+    } else if (this.operator === '/') {
+      if (this.result === '0') {
+        this.result = this.error;
+        this.operator = '';
+        return;
+      } else {
+        this.memoryResult = Number(this.memoryResult) / Number(this.result);
+      }
+    }
+    this.result = this.memoryResult.toString();
+    this.operator = '';
+  }
+
+  clearDisplay() {
+    this.result = '0';
+    this.memoryResult = 0;
+    this.operator = '';
+  }
+
+  insertDot() {
+    this.result += '.';
+  }
 }
